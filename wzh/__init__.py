3a3a6bd629349df1c5e49813b245365a82ac7b1d
zhonghao wu <zhonghao wu>
2017-08-17 23:09:48 -0400

    first

--- wzh/__init__.py
+++ wzh/__init__.py
@@ -5,10 +5,23 @@ try:
 except:
 	from blog import blog
 
+try:
+	from .blog.models import Post
+except:
+	from blog.models import Post
+
+from flask_admin.contrib.mongoengine import ModelView
+
 
 app = Flask(__name__)
 app.register_blueprint(blog, url_prefix='/blog')
-admin = Admin(app, name='microblog', template_mode='bootstrap3')
+admin = Admin(app, name='blog', template_mode='bootstrap3', url='/blogadmin')
+
+
+class PostView(ModelView):
+	column_filters = ['title']
+
+admin.add_view(PostView(Post))
 
 
 if __name__ == '__main__':
